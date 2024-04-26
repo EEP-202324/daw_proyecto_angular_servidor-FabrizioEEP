1a9b4daf0dea0d4eee4a60d61ce4247dc6b052f9
FabrizioEEP <145449016+FabrizioEEP@users.noreply.github.com>
2024-04-26 07:18:54 +0200

    Creación del Componente "home"

---
+++ ProyectoFinal/src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { PabellonLocationComponent } from '../pabellon-location/pabellon-location.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
