e72d8e948d8e3a6200dc170697db3f6924ccd0fc
FabrizioEEP <145449016+FabrizioEEP@users.noreply.github.com>
2024-04-27 15:45:28 +0200

    Cambios para que compile
    Se generó el error "RangeError: Maximum stack size exceeded". He tenido que hacer ciertos cambios para que vuelva a compilar.

--- ProyectoFinal/src/app/app-routing.module.ts
+++ ProyectoFinal/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PabellonLocationComponent } from './pabellon-location/pabellon-location.component';
+// import { PabellonLocationComponent } from './pabellon-location/pabellon-location.component';
 
 
 const routes: Routes = [];
@@ -9,8 +9,9 @@ const routes: Routes = [];
 @NgModule({
   imports: [RouterModule.forRoot(routes),
   CommonModule,
-    PabellonLocationComponent,
+    // PabellonLocationComponent,
   ],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
+
--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { PabellonLocationComponent } from './pabellon-location/pabellon-location.component';
+// import { HomeComponent } from './home/home.component';
+// import { PabellonLocationComponent } from './pabellon-location/pabellon-location.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    PabellonLocationComponent
+    // HomeComponent,
+    // PabellonLocationComponent
   ],
   imports: [
     BrowserModule,
