c2a860985889f647c0946bf0410016e8ab2196fe
FabrizioEEP <145449016+FabrizioEEP@users.noreply.github.com>
2024-04-28 11:44:56 +0200

    Creación del componente "main"  y  creación del componente "home", otra vez

--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
 // import { PabellonLocationComponent } from './pabellon-location/pabellon-location.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // HomeComponent,
+    MainComponent,
     // PabellonLocationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeComponent,
   ],
   providers: [
     provideClientHydration()
