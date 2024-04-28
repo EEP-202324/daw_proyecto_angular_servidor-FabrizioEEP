09bb50e3adb8eceef3ddf544ee9c00a29d19cace
FabrizioEEP <145449016+FabrizioEEP@users.noreply.github.com>
2024-04-28 11:50:15 +0200

    Eliminación de "appmodule"
    Debido a la creación del componente "main" y al estar utilizando "standalone".

--- ProyectoFinal/src/main.ts
+++ ProyectoFinal/src/main.ts
@@ -1,7 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { MainComponent } from './app/main/main.component';
 
-import { AppModule } from './app/app.module';
+bootstrapApplication(MainComponent)  // Para que inicie desde el componente main
+.catch(err => console.error(err));
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
