26c405b1ec3218437b609a0af9ea2c14b0d69a9f
FabrizioEEP <145449016+FabrizioEEP@users.noreply.github.com>
2024-04-30 18:45:00 +0200

    Eliminación del proyecto
    Debido a la utilización del standalone.

--- ProyectoFinal/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { MainComponent } from './app/main/main.component';
-
-bootstrapApplication(MainComponent)  // Para que inicie desde el componente main
-.catch(err => console.error(err));
-
-
-
