b85369ab757cb2a8d976a401022c52ca410a706e
FabrizioEEP <145449016+FabrizioEEP@users.noreply.github.com>
2024-05-10 13:06:51 +0200

    Creación de datos de pabellones

--- first-app-lesson-00/src/app/home/home.component.ts
+++ first-app-lesson-00/src/app/home/home.component.ts
@@ -16,15 +16,50 @@ import { AtributosPabellon, FuncionalidadTipo } from '../atributos-pabellon';
 export class HomeComponent {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
-  atributosPabellon: AtributosPabellon = {
-    id: 9999,
-    nombre: 'Test Pabellón',
-    ubicacion: 12,
-    aforo: 2000,
-    funcionalidad: FuncionalidadTipo.Aula,
-    disponibilidad: true,
-    photo: `${this.baseUrl}/example-house.jpg`,
-    // serviciosAdicionales: ['hola', 'como'],
-    fechaDisponibilidad: new Date('2002-12-12')
-  };
+  // atributosPabellon: AtributosPabellon = {
+  //   id: 9999,
+  //   nombre: 'Test Pabellón',
+  //   ubicacion: 12,
+  //   aforo: 2000,
+  //   funcionalidad: FuncionalidadTipo.Aula,
+  //   disponibilidad: true,
+  //   photo: `${this.baseUrl}/example-house.jpg`,
+  //   // serviciosAdicionales: ['hola', 'como'],
+  //   fechaDisponibilidad: new Date('2002-12-12')
+  // };
+
+  atributosPabellon: AtributosPabellon[] = [
+    {
+      id: 9999,
+      nombre: 'Test Pabellón 1',
+      ubicacion: 12,
+      aforo: 2000,
+      funcionalidad: FuncionalidadTipo.Aula,
+      disponibilidad: true,
+      photo: `${this.baseUrl}/example-house1.jpg`,
+      fechaDisponibilidad: new Date('2002-12-12')
+    },
+    {
+      id: 10001,
+      nombre: 'Test Pabellón 2',
+      ubicacion: 15,
+      aforo: 1500,
+      funcionalidad: FuncionalidadTipo.Auditorio,
+      disponibilidad: false,
+      photo: `${this.baseUrl}/example-house2.jpg`,
+      fechaDisponibilidad: new Date('2003-05-25')
+    },
+    {
+      id: 10002,
+      nombre: 'Test Pabellón 3',
+      ubicacion: 20,
+      aforo: 1800,
+      funcionalidad: FuncionalidadTipo.Laboratorio,
+      disponibilidad: true,
+      photo: `${this.baseUrl}/example-house3.jpg`,
+      fechaDisponibilidad: new Date('2004-08-10')
+    }
+  ];
 }
+
+
