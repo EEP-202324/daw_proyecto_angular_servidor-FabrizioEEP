4bac2fb12d1bf37afc43925793687d1833394d15
FabrizioEEP <145449016+FabrizioEEP@users.noreply.github.com>
2024-05-10 11:42:12 +0200

    Actualización interfaz atributosPabellon
    - Atributos agregados

--- first-app-lesson-00/src/app/atributos-pabellon.ts
+++ first-app-lesson-00/src/app/atributos-pabellon.ts
@@ -1,2 +1,18 @@
 export interface AtributosPabellon {
+  id: number;
+  nombre: string;
+  ubicacion: number;
+  aforo: number;
+  funcionalidad: FuncionalidadTipo;
+  disponibilidad: boolean;
+  photo: string;
+  serviciosAdicionales: string[];
+  fechaDisponibilidad: Date;
+}
+
+export enum FuncionalidadTipo {
+  Auditorio = 'Auditorio',
+  Aula = 'Aula',
+  Laboratorio = 'Laboratorio',
+  Otro = 'Otro',
 }
