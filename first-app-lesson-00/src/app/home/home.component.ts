6d852bb045e6bc1a2961b84f4f571c069f836ed3
FabrizioEEP <145449016+FabrizioEEP@users.noreply.github.com>
2024-05-10 11:49:52 +0200

    Creación de pabellon de prueba

--- first-app-lesson-00/src/app/home/home.component.ts
+++ first-app-lesson-00/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AtributosPabellonComponent } from '../atributos-pabellon/atributos-pabellon.component';
+import { AtributosPabellon, FuncionalidadTipo } from '../atributos-pabellon';
 
 @Component({
   selector: 'app-home',
@@ -13,5 +14,17 @@ import { AtributosPabellonComponent } from '../atributos-pabellon/atributos-pabe
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  atributosPabellon: AtributosPabellon = {
+    id: 9999,
+    nombre: 'Test Pabellón',
+    ubicacion: 12,
+    aforo: 2000,
+    funcionalidad: FuncionalidadTipo.Aula,
+    disponibilidad: true,
+    photo: `${this.baseUrl}/example-house.jpg`,
+    serviciosAdicionales: ['hola', 'como'],
+    fechaDisponibilidad: new Date('2002-12-12')
+  };
 }
