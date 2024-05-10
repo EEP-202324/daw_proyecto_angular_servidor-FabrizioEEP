79fd4128e83c013bfb963afbef366ae8bedb9292
FabrizioEEP <145449016+FabrizioEEP@users.noreply.github.com>
2024-05-11 00:53:08 +0200

    Implementación Routerlink

--- first-app-lesson-00/src/app/atributos-pabellon/atributos-pabellon.component.ts
+++ first-app-lesson-00/src/app/atributos-pabellon/atributos-pabellon.component.ts
@@ -1,11 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AtributosPabellon } from '../atributos-pabellon';
+import { RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-atributos-pabellon',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+  ],
   templateUrl: './atributos-pabellon.component.html',
   styleUrl: './atributos-pabellon.component.css'
 })
