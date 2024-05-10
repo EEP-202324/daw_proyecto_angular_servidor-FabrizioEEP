7d5dc1b9048d69d9e5f5d7a538f01698734d688f
FabrizioEEP <145449016+FabrizioEEP@users.noreply.github.com>
2024-05-10 11:16:36 +0200

    Actualización home.component
    - Import "CommonModule" en .ts
    - Import "AtributosPabellonComponent" en .ts
    - Cambio "home.component.html"

--- first-app-lesson-00/src/app/home/home.component.ts
+++ first-app-lesson-00/src/app/home/home.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AtributosPabellonComponent } from '../atributos-pabellon/atributos-pabellon.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    AtributosPabellonComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
