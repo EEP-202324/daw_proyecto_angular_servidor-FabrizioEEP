9bd3457c199a3bbfb465cf83d7bf1bfc02bd671f
FabrizioEEP <145449016+FabrizioEEP@users.noreply.github.com>
2024-05-10 11:03:23 +0200

    Componente "atributos-pabellon" creado

---
+++ first-app-lesson-00/src/app/atributos-pabellon/atributos-pabellon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atributos-pabellon',
+  standalone: true,
+  imports: [],
+  templateUrl: './atributos-pabellon.component.html',
+  styleUrl: './atributos-pabellon.component.css'
+})
+export class AtributosPabellonComponent {
+
+}
