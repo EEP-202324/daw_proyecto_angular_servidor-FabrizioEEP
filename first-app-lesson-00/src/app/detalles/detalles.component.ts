81cfb394868209c2a4cd37cc5286480a64068696
FabrizioEEP <145449016+FabrizioEEP@users.noreply.github.com>
2024-05-11 16:49:45 +0200

    Agregue las funciones del formulario a la página de detalles

--- first-app-lesson-00/src/app/detalles/detalles.component.ts
+++ first-app-lesson-00/src/app/detalles/detalles.component.ts
@@ -2,12 +2,16 @@ import { Component, Inject, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { ServicioPabellonService } from '../servicio-pabellon.service';
-import { AtributosPabellon } from '../atributos-pabellon';
+import { AtributosPabellon, FuncionalidadTipo } from '../atributos-pabellon';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-detalles',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './detalles.component.html',
   styleUrl: './detalles.component.css'
 })
@@ -16,9 +20,30 @@ export class DetallesComponent {
   servicioPabellonService = inject(ServicioPabellonService);
   atributosPabellon: AtributosPabellon | undefined;
 
+  formularioSolicitud = new FormGroup({
+    nombre: new FormControl(''),
+    ubicacion: new FormControl(''),
+    aforo: new FormControl(0),
+    funcionalidad: new FormControl(FuncionalidadTipo.Aula),
+    disponibilidad: new FormControl(false),
+    photo: new FormControl(''),
+    fechaDisponibilidad: new FormControl(new Date())
+  });
+
   constructor() {
       const atributosPabellonId = Number(this.route.snapshot.params['id']);
       this.atributosPabellon =
   this.servicioPabellonService?.getPabellon(atributosPabellonId);
+  }
+  enviarSolicitud() {
+    this.servicioPabellonService.agregarPabellon(
+      this.formularioSolicitud.value.nombre ?? '',
+      this.formularioSolicitud.value.ubicacion ?? '',
+      this.formularioSolicitud.value.aforo ?? 0,
+      this.formularioSolicitud.value.funcionalidad ?? FuncionalidadTipo.Aula,
+      this.formularioSolicitud.value.disponibilidad ?? false,
+      this.formularioSolicitud.value.photo ?? '',
+      this.formularioSolicitud.value.fechaDisponibilidad ?? new Date(),
+    );
   }
 }
