7638e7a072baa21e28a4ba7a037baea0c40f099c
FabrizioEEP <FAlberto-Sanchez1@eep-igroup.com>
2024-05-13 10:16:01 +0200

    miPrimerTest creado

---
+++ ProyectoFinal_Spring/pabellon/src/test/java/com/example/pabellon/PabellonJsonTest.java
@@ -0,0 +1,12 @@
+package com.example.pabellon;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class PabellonJsonTest {
+	@Test
+	void miPrimerTest() {
+		assertThat(1).isEqualTo(42);
+	}
+}
