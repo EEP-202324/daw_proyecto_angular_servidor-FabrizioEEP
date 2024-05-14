a406c8bea9a3d1b0a1b45638bdf3e66360b1a43a
FabrizioEEP <FAlberto-Sanchez1@eep-igroup.com>
2024-05-14 11:37:10 +0200

    findById agregado y shouldReturnAPabellonWhenDataIsSavedTest funciona

--- ProyectoFinal_Spring/pabellon/src/main/java/com/example/pabellon/PabellonController.java
+++ ProyectoFinal_Spring/pabellon/src/main/java/com/example/pabellon/PabellonController.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-public class PabellonController {
 
+@RestController
+@RequestMapping("/pabellon")
+public class PabellonController {
+	
+	@GetMapping("/{requestId}")
+	private ResponseEntity<String> findById() {
+		return ResponseEntity.ok("{}");
+		}
 }
