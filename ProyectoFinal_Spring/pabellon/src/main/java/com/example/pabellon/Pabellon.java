1677ff19a898d98cf8e8cfe32fea388918eb2362
FabrizioEEP <FAlberto-Sanchez1@eep-igroup.com>
2024-05-14 12:09:31 +0200

    Actualización Pabellon

--- ProyectoFinal_Spring/pabellon/src/main/java/com/example/pabellon/Pabellon.java
+++ ProyectoFinal_Spring/pabellon/src/main/java/com/example/pabellon/Pabellon.java
@@ -14,7 +14,7 @@ public class Pabellon {
 	    private Date fechaDisponibilidad;
 
 	    public Pabellon(int id, String nombre, String ubicacion, int aforo, FuncionalidadTipo funcionalidad,
-			boolean disponibilidad, String foto) {
+			boolean disponibilidad, String foto, String photo) {
 		this.id = id;
 		this.nombre = nombre;
 		this.ubicacion = ubicacion;
@@ -23,4 +23,76 @@ public class Pabellon {
 		this.disponibilidad = disponibilidad;
 		this.photo = photo;
 	}   
+
+	    public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public String getUbicacion() {
+			return ubicacion;
+		}
+
+		public void setUbicacion(String ubicacion) {
+			this.ubicacion = ubicacion;
+		}
+
+		public int getAforo() {
+			return aforo;
+		}
+
+		public void setAforo(int aforo) {
+			this.aforo = aforo;
+		}
+
+		public FuncionalidadTipo getFuncionalidad() {
+			return funcionalidad;
+		}
+
+		public void setFuncionalidad(FuncionalidadTipo funcionalidad) {
+			this.funcionalidad = funcionalidad;
+		}
+
+		public boolean isDisponibilidad() {
+			return disponibilidad;
+		}
+
+		public void setDisponibilidad(boolean disponibilidad) {
+			this.disponibilidad = disponibilidad;
+		}
+
+		public String getPhoto() {
+			return photo;
+		}
+
+		public void setPhoto(String photo) {
+			this.photo = photo;
+		}
+
+		public Date getFechaDisponibilidad() {
+			return fechaDisponibilidad;
+		}
+
+		public void setFechaDisponibilidad(Date fechaDisponibilidad) {
+			this.fechaDisponibilidad = fechaDisponibilidad;
+		}
+		
+
+		@Override
+	public String toString() {
+		return "Pabellon [id=" + id + ", nombre=" + nombre + ", ubicacion=" + ubicacion + ", aforo=" + aforo
+				+ ", funcionalidad=" + funcionalidad + ", disponibilidad=" + disponibilidad + ", photo=" + photo
+				+ ", fechaDisponibilidad=" + fechaDisponibilidad + "]";
+	}
 }
