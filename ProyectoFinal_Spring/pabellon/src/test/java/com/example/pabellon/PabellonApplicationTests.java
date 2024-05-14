fbb48dbaa6ba79b2f298dcc4f0521ae9fb093caa
FabrizioEEP <FAlberto-Sanchez1@eep-igroup.com>
2024-05-14 11:22:39 +0200

    shouldReturnAPabellonWhenDataIsSavedTest creado

--- ProyectoFinal_Spring/pabellon/src/test/java/com/example/pabellon/PabellonApplicationTests.java
+++ ProyectoFinal_Spring/pabellon/src/test/java/com/example/pabellon/PabellonApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.pabellon;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PabellonApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void shouldReturnAPabellonWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/pabellon/99", String.class);
 
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 }
