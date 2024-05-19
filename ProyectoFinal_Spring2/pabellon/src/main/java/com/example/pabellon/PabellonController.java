a4e8c36b8e961c0761109d87522a2bef39c9497a
FabrizioEEP <FAlberto-Sanchez1@eep-igroup.com>
2024-05-19 19:35:10 +0200

    Origing Agregado

--- ProyectoFinal_Spring2/pabellon/src/main/java/com/example/pabellon/PabellonController.java
+++ ProyectoFinal_Spring2/pabellon/src/main/java/com/example/pabellon/PabellonController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/pabellones")
 public class PabellonController {
